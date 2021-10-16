1456ddd99cca8afe4bd6db362ef9b497fad6198a
嘟嘟 <2491332780@qq.com>
2021-10-16 18:31:13 +0800

    分支添加

--- helloGit/src/main/java/git/HelloGit.java
+++ helloGit/src/main/java/git/HelloGit.java
@@ -7,6 +7,6 @@ package git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello  Git !");
-        System.out.println("主干添加");
+        System.out.println("分支添加");
     }
 }
